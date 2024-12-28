78bf738438b0a7b74eae255b3d2732b16ae8231d
zdn <library.devz@gmail.com>
2024-12-28 09:20:04 +0700

    delete unused code

--- middlewares/cloudinary.storage.js
+++ middlewares/cloudinary.storage.js
@@ -6,22 +6,21 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const storageCloudinary = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "bwa-staycation/images", // Nama folder di Cloudinary
-    allowed_formats: ["jpg", "jpeg", "png", "gif"], // Format gambar yang diperbolehkan
+    folder: "bwa-staycation/images",
+    allowed_formats: ["jpg", "jpeg", "png", "gif"],
   },
 });
 
 // Fungsi upload untuk satu file
 const upload = multer({
   storage: storageCloudinary,
-  limits: { fileSize: 5000000 }, // Batasi ukuran file, misalnya 1MB
-}).single("image"); // Menggunakan "image" sebagai field untuk single file
+  limits: { fileSize: 5000000 },
+}).single("image");
 
 // Fungsi upload untuk banyak file
 const uploadMultiple = multer({
   storage: storageCloudinary,
-  limits: { fileSize: 5000000 }, // Batasi ukuran file, misalnya 1MB
-}).array("images", 12); // "images" adalah nama field pada form dan 12 adalah jumlah maksimal file yang di-upload
+  limits: { fileSize: 5000000 },
+}).array("images", 12);
 
-// Menggabungkan keduanya dalam satu ekspor
 module.exports = { upload, uploadMultiple };
